492e9b7516be148482d5fa96160e20ed82b789f5
Daniel <floricela999@gmail.com>
2024-02-14 21:40:01 +0100

    first comit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Layout from './components/Layout/Layout';
+
+function App() {
+  return (
+    <div>
+      <Layout>
+      <h1>Resturant Website</h1>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,16 @@
+import { AppBar, Box, Typography } from '@mui/material'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <Box>
+        <AppBar>
+            <Typography>My Resturant</Typography>
+        </AppBar>
+    </Box>
+    </>
+  )
+}
+
+export default Header
