ded5d46efaad9ad7c5178e9b50ea2ef077cf2254
Flori Cela <floricela999@gmail.com>
2024-02-15 15:57:37 +0100

    Project finish

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,23 +1,11 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Menu from './pages/Menu';
+import Layout from '../components/Layout/Layout';
 
 const PageNotFound = () => {
   return (
-    <div>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/menu' element={<Menu />} />
-        <Route path='*' element={<PageNotFound />} />
-      </Routes>
-      </BrowserRouter>
-    </div>
+    <Layout>
+      <h1>Page Not Found</h1>
+    </Layout>
   )
 }
 
